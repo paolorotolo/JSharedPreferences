d61d97363c39fd8d0c2d2892c5b1800942dd13cf
Paolo Rotolo <paolorotolo@ubuntu.com>
2014-09-01 21:33:53 +0200

    Initial Release

---
+++ Storage.java
@@ -0,0 +1,35 @@
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.webkit.JavascriptInterface;
+
+public class Storage {
+    private Context mContext;
+    private SharedPreferences mStorage;
+
+
+    Storage(Context c) {
+        mContext = c;
+        mStorage = c.getSharedPreferences("Storage", 0);
+    }
+
+    @JavascriptInterface
+    public String getItem(String key) {
+        return mStorage.getString(key, null);
+    }
+
+    @JavascriptInterface
+    public void setItem(String key, String value){
+        SharedPreferences.Editor e = mStorage.edit();
+
+        e.putString(key, value);
+        e.apply();
+    }
+
+    @JavascriptInterface
+    public void removeItem(String key) {
+        SharedPreferences.Editor e = mStorage.edit();
+
+        e.remove(key);
+        e.apply();
+    }
+}
